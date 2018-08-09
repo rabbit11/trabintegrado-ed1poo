d9aa7dbe7856b88b855e33bd8554ef2f7630afc8
Natham <nathamcoracini@gmail.com>
2018-08-09 13:51:10 -0300

    .

--- palavra.h
+++ palavra.h
@@ -9,17 +9,17 @@ using namespace std;
 class Palavra {
 
 public:
-	Palavra(string palavra = "") : word(palavra) {ocorrencias = 0;};
+	Palavra(wstring palavra = "") : word(palavra), ocorrencias(0);
 	bool operator== (const Palavra &);
 	int compara(Palavra &);
-	string getWord() const;
+	wstring getWord() const;
 	void setWord(const string&);
 	friend ostream & operator << (ostream &out, const Palavra &);
 	int getOcorrencias() const;
 	void incrementarOcorrencias();
 
 private:
-	string word;
+	wstring word;
 	int ocorrencias;
 };
 
